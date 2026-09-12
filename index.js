662a7521d140a23d34b52aa2d7b3086614bdce05
Parineeta <parineeta@Parineetas-MacBook-Air.local>
2026-09-12 21:57:03 +0530

    Refactor index.js to act purely as an entry point wiring modules together

--- index.js
+++ index.js
@@ -1,99 +1,20 @@
-const { spawn } = require('child_process');
 const path = require('path');
-const setupKeyControls = require('./keyHandler');
 const loadSongs = require('./songLoader');
+const setupKeyControls = require('./keyHandler');
+const controller = require('./controller');
 
-// We replaced 10 lines of file-system code with this single line!
+// 1. Get the data
 const songs = loadSongs();
 const songsFolder = path.join(__dirname, 'songs');
 
-let currentAudioProcess = null;
-let selectedIndex = 0;
-let currentlyPlaying = "---";
-let isPaused = false; 
-let isFirstRender = true;
+// 2. Hand the data over to the controller
+controller.init(songs, songsFolder);
 
-function renderMenu() {
-  if (!isFirstRender) {
-    const linesToMoveUp = songs.length + 3;
-    process.stdout.write(`\x1B[${linesToMoveUp}A`);
-  }
-  isFirstRender = false;
-
-  console.log('--- Terminal Music Player ---');
-  
-  songs.forEach((song, index) => {
-    if (index === selectedIndex) {
-      console.log(`  \x1b[36m> ${song}\x1b[0m`);
-    } else {
-      console.log(`    ${song}`);
-    }
-  });
-  
-  console.log('-----------------------------');
-  
-  const status = isPaused ? `[PAUSED] ${currentlyPlaying}` : currentlyPlaying;
-  console.log(`Now playing: ${status}                                        `);
-}
-
-function playSong() {
-  if (currentAudioProcess) {
-    currentAudioProcess.kill();
-  }
-
-  const songName = songs[selectedIndex];
-  const songPath = path.join(songsFolder, songName);
-  
-  currentlyPlaying = songName;
-  isPaused = false; 
-  
-  currentAudioProcess = spawn('afplay', [songPath]);
-  
-  renderMenu();
-}
-
-function handleUp() {
-  if (selectedIndex > 0) {
-    selectedIndex--;
-    renderMenu();
-  }
-}
-
-function handleDown() {
-  if (selectedIndex < songs.length - 1) {
-    selectedIndex++;
-    renderMenu();
-  }
-}
-
-function handleEnter() {
-  playSong();
-}
-
-function handleSpace() {
-  if (!currentAudioProcess) {
-    return;
-  }
-
-  if (isPaused) {
-    currentAudioProcess.kill('SIGCONT');
-    isPaused = false;
-  } else {
-    currentAudioProcess.kill('SIGSTOP');
-    isPaused = true;
-  }
-  
-  renderMenu();
-}
-
-function handleQuit() {
-  if (currentAudioProcess) {
-    currentAudioProcess.kill();
-  }
-  console.clear();
-  process.exit(0);
-}
-
-setupKeyControls(handleUp, handleDown, handleEnter, handleQuit, handleSpace);
-
-renderMenu();
+// 3. Connect the keyboard straight to the controller's functions
+setupKeyControls(
+  controller.handleUp,
+  controller.handleDown,
+  controller.handleEnter,
+  controller.handleQuit,
+  controller.handleSpace
+);
